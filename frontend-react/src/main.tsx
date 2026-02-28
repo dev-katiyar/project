501e2965584a13bbe49e667989548fd2ed215e6f
Abhilash <developer.katiyar@gmail.com>
2026-02-28 14:28:48 -0700

    stremlined themes and relaated styles

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Styles
-import "./styles/themes.css";
-
-// PrimeReact theme & core (imported here so Vite bundles them)
+// PrimeReact theme & core (loaded first so our overrides win in cascade)
 import "primereact/resources/themes/lara-dark-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+
+// Custom theme variables & component overrides (loaded after PrimeReact
+// so [data-theme] variable bridges beat PrimeReact's :root definitions)
+import "./styles/themes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
